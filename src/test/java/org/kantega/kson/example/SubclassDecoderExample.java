7bb8486aedf2535c143b24b8d035ba3b4e8623a5
Atle Prange <atle.prange@gmail.com>
2018-08-09 10:41:27 +0200

    Added example of decodeing subclasses

---
+++ src/test/java/org/kantega/kson/example/SubclassDecoderExample.java
@@ -0,0 +1,83 @@
+package org.kantega.kson.example;
+
+import fj.data.List;
+import org.kantega.kson.JsonResult;
+import org.kantega.kson.codec.JsonDecoder;
+import org.kantega.kson.json.JsonValue;
+import org.kantega.kson.parser.JsonParser;
+
+import java.math.BigDecimal;
+
+import static org.kantega.kson.codec.JsonDecoders.*;
+
+public class SubclassDecoderExample {
+
+    static final JsonDecoder<Common> aDecoder =
+        obj(field("name", stringDecoder))
+            .map(A::new);
+
+    static final JsonDecoder<Common> bDecoder =
+        obj(field("value", bigDecimalDecoder))
+            .map(B::new);
+
+    static final JsonDecoder<Common> commonDecoder =
+        new SubclassDecoder();
+
+    static final JsonDecoder<List<Common>> commonListDecoder =
+        arrayDecoder(commonDecoder);
+
+    static final String json = "[" +
+        "{\"type\":\"a\",\"name\":\"ONE\"}," +
+        "{\"type\":\"a\",\"name\":\"TWO\"}," +
+        "{\"type\":\"b\",\"value\":1.2}]";
+
+    static final String failjson = "[" +
+        "{\"type\":\"a\",\"name\":\"ONE\"}," +
+        "{\"type\":\"a\",\"name\":\"TWO\"}," +
+        "{\"type\":\"c\",\"value\":1.2}]";
+
+    public static void main(String[] args) {
+        List<Common> list = JsonParser.parse(json).decode(commonListDecoder).orThrow();
+        System.out.println(list);
+
+        List<Common> faillist = JsonParser.parse(failjson).decode(commonListDecoder).orThrow();
+        System.out.println(faillist);
+    }
+
+
+    static class SubclassDecoder implements JsonDecoder<Common> {
+
+
+        @Override
+        public JsonResult<Common> decode(JsonValue v) {
+            return v.fieldAsText("type").bind(type -> {
+                if (type.equals("a"))
+                    return aDecoder.decode(v);
+                else if (type.equals("b"))
+                    return bDecoder.decode(v);
+                else
+                    return JsonResult.fail("Unknown type");
+            });
+        }
+    }
+
+    interface Common {
+    }
+
+    static class A implements Common {
+
+        public final String name;
+
+        A(String name) {
+            this.name = name;
+        }
+    }
+
+    static class B implements Common {
+        public final BigDecimal value;
+
+        B(BigDecimal value) {
+            this.value = value;
+        }
+    }
+}
